0a510860db10b406c7ae0e8afa5e5cd66ef65b14
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-07-08 20:33:22 +0530

    next auth done

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,12 @@ import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import Email from "next-auth/providers/email";
+import bcrypt from 'bcrypt'
+
+import { ConnectData } from "./db";
+import user from "../models/User";
+import { error, log } from "console";
+import { Session } from "inspector/promises";
 
 export const authOptions:NextAuthOptions={
  
@@ -22,21 +28,64 @@ export const authOptions:NextAuthOptions={
             password: { label: "Password", type: "password" }
           },
           async authorize(credentials, req) {
-            // Add logic here to look up the user from the credentials supplied
-            const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-      
-            if (user) {
-              // Any object returned will be saved in `user` property of the JWT
-              return user
-            } else {
-              // If you return null then an error will be displayed advising the user to check their details.
-              return null
-      
-              // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-            }
+                if(!credentials?.Email || !credentials.password){
+                    throw new Error("Missing Email or password")
+                }
+                try{
+                  await ConnectData()
+                  const User=await user.findOne({
+                    email:credentials.Email
+
+                  })
+                  if(!User){
+                    throw new Error("No User Found")
+                  }
+
+                 const Isvalid= await bcrypt.compare(
+                    credentials.password,
+                    User.password
+                  )
+
+                  if(!Isvalid){
+                    throw new Error("Invalid Password");
+                  }
+
+                  return {
+                    id:User._id.toString(),
+                    email:User.Email
+                  }
+                }
+                catch(Error){
+                    console.log("Auth Error ");
+                    throw error
+                    
+                }
           }
     })  
-
-   ]
+    
+   ],
+   callbacks:{
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({session, token }) {
+            if (session.user) {
+               session.user.id= token.id 
+            }
+            return session;
+        }
+   },
+   pages:{
+    signIn:"/login",
+    error:"/login"
+   },
+   session:{
+    strategy:"jwt",
+    maxAge:30*24*60*60
+   },
+   secret:process.env.NEXTAUTH_SECRET
 
 };
